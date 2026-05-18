bd81fb8048e58a0665a177d464ffdd5f31d59a88
bartur-code <b.artur@escolar.ifrn.edu.br>
2026-05-18 14:29:57 +0000

    Rec1

---
+++ Atividade_03/Recuperacao/exercicio1.py
@@ -0,0 +1,17 @@
+# Usuário
+# class UI = user interface > Prints e Inputs
+#Entidade
+
+# OBS.: O diagrama não muda independente da linguagem
+class Frete:
+    def __init__(self, d : float, p : float): # construtor (todos os dados) # retorna a variável "Frete"
+    def set_distancia(self, d : float): # no diagrmama tem o "void" que não retorna nenhum valor
+    def set_peso(self, p : float): # no diagrmama tem o "void" que não retorna nenhum valor
+    def get_distancia(self): # não tem "void", logo tem que ter o "return" em baixo
+        return
+    def get_peso(self): # não tem "void", logo tem que ter o "return" em baixo
+        return
+    def calc_frete(self): # não tem "void", logo tem que ter o "return" em baixo
+        return 
+    def __str__(self): # mostrar os dados (todos) # não tem "void", logo tem que ter o "return" em baixo
+        return
